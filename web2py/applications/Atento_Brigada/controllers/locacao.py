ade1331c53aebd98a0feebeae6a33e7be15d1daa
Hugo Marques <hugomarques@tecsus.com.br>
2016-09-21 21:25:41 -0300

    [Closes #7] Finalização das caixas Servidor recebe informaçoes, caixa cria mostra e destroi.

--- web2py/applications/Atento_Brigada/controllers/locacao.py
+++ web2py/applications/Atento_Brigada/controllers/locacao.py
@@ -1,3 +1,5 @@
+import json
+
 def alocar():
     query= db(db.planta).select().first()
     plantaurl = URL('cadastros', 'download', args=query.image)
@@ -7,4 +9,8 @@ def alocar_referencia():
     query= db(db.planta).select().first()
     plantaurl = URL('cadastros', 'download', args=query.image)
     return dict(plantaurl=plantaurl)
+
+def salvar_caixa():
+    listaCaixa =json.loads(request.vars.listaCaixa)
+    pass
 
